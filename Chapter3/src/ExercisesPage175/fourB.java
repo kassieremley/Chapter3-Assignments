e8c95e68a0626a6259fba474f81acf3b6782b14c
kassieremley <remleykasandra@yahoo.com>
2017-02-08 13:40:29 -0600

    djbklu

--- Chapter3/src/ExercisesPage175/fourB.java
+++ Chapter3/src/ExercisesPage175/fourB.java
@@ -1,20 +1,49 @@
-/**
- * 
- */
 package ExercisesPage175;
-
-/**
- * @author kr131457
- *
- */
+import java.util.Scanner;
 public class fourB {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args)
+	{
+	   int numOne;
+	   int numDouble;
+	   int numPlusFive;
+	   int numSquared;
+	   
+	   Scanner keyboard = new Scanner(System.in);
+	   
+	   System.out.println("Enter a number>>> ");
+	   numOne = keyboard.nextInt();
+	   
+	   //System.out.println("Enter your second number>>> ");
+	   //numTwo = keyboard.nextInt();
+	   
+	   numDouble = displayTwiceNumber(numOne);
+	   numPlusFive = displayNumberPlusFive(numOne);
+	   numSquared = displayNumberSquared(numOne);
+	   
+	   System.out.println("Your number double is " + numDouble);
+	   System.out.println("Your number plus 5 is " + numPlusFive);
+	   System.out.println("Your number squared is " + numSquared);
 	}
+	  public static int displayTwiceNumber(int num)
+	{
+		int numDouble;
+		numDouble = num * 2;
+		return numDouble;
+	}
+	  public static int displayNumberPlusFive(int num)
+   {
+		int numPlusFive;
+		numPlusFive = num + 5;
+		return numPlusFive;
+	  }
+	    public static int displayNumberSquared(int num)
+	  {
+	    int numSquared;
+		numSquared = num * num;
+	    return numSquared;
+		  
+	  }
 
 }
